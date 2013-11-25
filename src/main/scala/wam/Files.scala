9d71b85fb0a89667c124e3641307c4d09d670d07
Teo Klestrup Röijezon <teo@nullable.se>
2013-11-25 09:50:08 +0100

    Added defaults to wam.Files.walkFileTree, now actually obeys options and maxDepth

--- src/main/scala/wam/Files.scala
+++ src/main/scala/wam/Files.scala
@@ -3,6 +3,7 @@ package wam
 import java.nio.file.{Files => NioFiles, FileVisitor, FileVisitOption, FileVisitResult, Path}
 import java.nio.file.attribute.BasicFileAttributes
 import java.io.IOException
+import scala.collection.JavaConversions._
 
 
 /**
@@ -10,13 +11,16 @@ import java.io.IOException
  */
 trait Files {
   def walkFileTree(start: Path,
-                   node: (Path, BasicFileAttributes) => FileVisitResult,
-                   nodeFailed: (Path, IOException) => FileVisitResult,
-                   preDirectory: (Path, BasicFileAttributes) => FileVisitResult,
-                   postDirectory: (Path, Option[IOException]) => FileVisitResult,
-                   options: Set[FileVisitOption],
-                   maxDepth: Int): Path =
-    NioFiles.walkFileTree(start, new FileVisitor[Path] {
+                   node: (Path, BasicFileAttributes) => FileVisitResult = (_, _) => FileVisitResult.CONTINUE,
+                   nodeFailed: (Path, IOException) => FileVisitResult = (_, e) => throw e,
+                   preDirectory: (Path, BasicFileAttributes) => FileVisitResult = (_, _) => FileVisitResult.CONTINUE,
+                   postDirectory: (Path, Option[IOException]) => FileVisitResult = {
+                     case (_, Some(e)) => throw e
+                     case (_, None) => FileVisitResult.CONTINUE
+                   },
+                   options: Set[FileVisitOption] = Set(),
+                   maxDepth: Int = Int.MaxValue): Path =
+    NioFiles.walkFileTree(start, options, maxDepth, new FileVisitor[Path] {
       def preVisitDirectory(path: Path, attrs: BasicFileAttributes): FileVisitResult = preDirectory(path, attrs)
 
       def visitFile(path: Path, attrs: BasicFileAttributes): FileVisitResult = node(path, attrs)
